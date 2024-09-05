738a87ed3cb183c08e4e97768af8b34b7afc3259
Ricks <bricks@unomaha.edu>
2024-09-05 13:19:09 -0500

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,11 @@
+print("One") # 1
+print("One") # 2
+for i in range(4):
+    print("Row") # 3-6
+print("One") # 7
+print("One") # 8
+print("One") # 9
+print("One") # 10
+print("One") # 11
+print("One") # 12
+print("One") # 13
